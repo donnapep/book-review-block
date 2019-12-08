5c643ce349b1214ba2ab92013cb6e5afa31984c5
Donna Peplinskie <donnapep@gmail.com>
2019-12-08 06:04:18 -0500

    Deprecate version of block that stored attributes in post content

--- index.jsx
+++ index.jsx
@@ -34,9 +34,6 @@ const ratings = [
 	{ rating: 2, title: 'it was ok' },
 	{ rating: 1, title: 'did not like it' },
 ];
-function isOldVersion( attributes ) {
-	return attributes.details.length !== 0;
-}
 
 registerBlockType( 'book-review-block/book-review', {
 	title: 'Book Review',
@@ -120,72 +117,6 @@ registerBlockType( 'book-review-block/book-review', {
 			source: 'meta',
 			meta: 'book_review_title',
 		},
-		/* Old attributes for backwards compatibility */
-		/* If any of these have a value, then this is the old version. */
-		author: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__author',
-		},
-		details: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__details'
-		},
-		format: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__format',
-		},
-		genre: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__genre',
-		},
-		pages: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__pages',
-		},
-		publisher: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__publisher',
-		},
-		rating: {
-			type: 'string',
-		},
-		releaseDate: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__release-date',
-		},
-		series: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__series',
-		},
-		source: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__source',
-		},
-		summary: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__description',
-		},
-		title: {
-			type: 'array',
-			source: 'children',
-			selector: '.book-review-block__title',
-		},
-		url: {
-			type: 'string',
-			source: 'attribute',
-			selector: 'img',
-			attribute: 'src',
-		},
 	},
 
 	edit: ( { attributes, className, isSelected, setAttributes } ) => {
@@ -193,77 +124,41 @@ registerBlockType( 'book-review-block/book-review', {
 			alt,
 			backgroundColor,
 			id,
-		} = attributes;
-		const isOldBlock = isOldVersion( attributes );
-
-		// Set old (HTML) attributes to undefined so data will be stored to new (meta) attributes.
-		if ( ! isOldBlock ) {
-			attributes.author = undefined;
-			attributes.format = undefined;
-			attributes.genre = undefined;
-			attributes.pages = undefined;
-			attributes.publisher = undefined;
-			attributes.rating = undefined;
-			attributes.releaseDate = undefined;
-			attributes.series = undefined;
-			attributes.source = undefined;
-			attributes.summary = undefined;
-			attributes.title = undefined;
-			attributes.url = undefined;
-		}
-
-		// If HTML attributes are undefined, use the meta attributes.
-		const {
-			author: author = attributes.book_review_author,
-			format: format = attributes.book_review_format,
-			genre: genre = attributes.book_review_genre,
-			pages: pages = attributes.book_review_pages,
-			publisher: publisher = attributes.book_review_publisher,
-			rating: currentRating = attributes.book_review_rating,
-			releaseDate: releaseDate = attributes.book_review_release_date,
-			series: series = attributes.book_review_series,
-			source: source = attributes.book_review_source,
-			summary: summary = attributes.book_review_summary,
-			title: title = attributes.book_review_title,
-			url: coverUrl = attributes.book_review_cover_url,
+			book_review_author,
+			book_review_cover_url,
+			book_review_format,
+			book_review_genre,
+			book_review_pages,
+			book_review_publisher,
+			book_review_rating,
+			book_review_release_date,
+			book_review_series,
+			book_review_source,
+			book_review_summary,
+			book_review_title,
+			// Deprecated attributes
+			author,
+			format,
+			genre,
+			pages,
+			publisher,
+			rating,
+			releaseDate,
+			series,
+			source,
+			summary,
+			title,
+			url,
 		} = attributes;
 
-		const setCover = ( { alt, id, url } ) => {
-			if ( ! isOldBlock ) {
-				setAttributes( { alt, id, book_review_cover_url: url } );
-			} else {
-				setAttributes( { alt, id, url } );
-			}
-		}
-		const updateBackgroundColor = value => setAttributes( { backgroundColor: value } );
-		const updateRating = event => {
-			if ( ! isOldBlock ) {
-				setAttributes( { book_review_rating: event.target.dataset.rating } );
-			} else {
-				setAttributes( { rating: event.target.dataset.rating } );
-			}
-		}
-		const updateReleaseDate = releaseDate => {
-			if ( ! isOldBlock ) {
-				setAttributes( { book_review_release_date: releaseDate } );
-			} else {
-				setAttributes( { [ 'releaseDate' ]: releaseDate } );
-			}
-		}
-		const updateValue = field => value => {
-			if ( ! isOldBlock ) {
-				field = 'book_review_' + field;
-			}
+		// Workaround for deprecated block migrations not saving to meta.
+		// This is needed to restore data that was stored in HTML attributes (i.e. post content).
+		const currentRating = book_review_rating ? book_review_rating : rating;
+		const coverUrl = book_review_cover_url ? book_review_cover_url : url;
 
-			setAttributes( { [ field ]: value } );
-		}
-		const updateSummary = summary => {
-			if ( ! isOldBlock ) {
-				setAttributes( { book_review_summary: summary } );
-			} else {
-				setAttributes( { [ 'summary' ]: summary } );
-			}
-		}
+		const setCover = ( { alt, id, url } ) => setAttributes( { alt, id, book_review_cover_url: url } );
+		const updateRating = event => setAttributes( { book_review_rating: event.target.dataset.rating } );
+		const updateValue = field => value => setAttributes( { [ field ]: value } );
 
 		return (
 			<div
@@ -297,7 +192,7 @@ registerBlockType( 'book-review-block/book-review', {
 							colorSettings={ [
 								{
 									value: backgroundColor && backgroundColor.value,
-									onChange: updateBackgroundColor,
+									onChange: updateValue( 'backgroundColor' ),
 									label: __( 'Background Color' ),
 								},
 							] }
@@ -329,57 +224,57 @@ registerBlockType( 'book-review-block/book-review', {
 
 				<div className="book-review-block__details">
 					<RichText
-						onChange={ updateValue( 'title' ) }
+						onChange={ updateValue( 'book_review_title' ) }
 						placeholder={ __( 'Enter title…' ) }
-						value={ title }
+						value={ book_review_title ? book_review_title : title }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateValue( 'series' ) }
+						onChange={ updateValue( 'book_review_series' ) }
 						placeholder={ __( 'Enter series…' ) }
-						value={ series }
+						value={ book_review_series ? book_review_series : series }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateValue( 'author' ) }
+						onChange={ updateValue( 'book_review_author' ) }
 						placeholder={ __( 'Enter author…' ) }
-						value={ author }
+						value={ book_review_author ? book_review_author : author }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateValue( 'genre' ) }
+						onChange={ updateValue( 'book_review_genre' ) }
 						placeholder={ __( 'Enter genre…' ) }
-						value={ genre }
+						value={ book_review_genre ? book_review_genre : genre }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateValue( 'publisher' ) }
+						onChange={ updateValue( 'book_review_publisher' ) }
 						placeholder={ __( 'Enter publisher…' ) }
-						value={ publisher }
+						value={ book_review_publisher ? book_review_publisher : publisher }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateReleaseDate }
+						onChange={ updateValue( 'book_review_release_date' ) }
 						placeholder={ __( 'Enter release date…' ) }
-						value={ releaseDate }
+						value={ book_review_release_date ? book_review_release_date : releaseDate }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateValue( 'format' ) }
+						onChange={ updateValue( 'book_review_format' ) }
 						placeholder={ __( 'Enter format…' ) }
-						value={ format }
+						value={ book_review_format ? book_review_format : format }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateValue( 'pages' ) }
+						onChange={ updateValue( 'book_review_pages' ) }
 						placeholder={ __( 'Enter pages…' ) }
-						value={ pages }
+						value={ book_review_pages ? book_review_pages : pages }
 						keepPlaceholderOnFocus />
 
 					<RichText
-						onChange={ updateValue( 'source' ) }
+						onChange={ updateValue( 'book_review_source' ) }
 						placeholder={ __( 'Enter source…' ) }
-						value={ source }
+						value={ book_review_source ? book_review_source : source }
 						keepPlaceholderOnFocus />
 
 					<div
@@ -398,9 +293,9 @@ registerBlockType( 'book-review-block/book-review', {
 
 					<RichText
 						multiline="p"
-						onChange={ updateSummary }
+						onChange={ updateValue( 'book_review_summary' ) }
 						placeholder={ __( 'Enter description…' ) }
-						value={ summary }
+						value={ book_review_summary ? book_review_summary : summary }
 						wrapperClassName="book-review-block__description"
 						inlineToolbar
 						keepPlaceholderOnFocus />
@@ -409,68 +304,141 @@ registerBlockType( 'book-review-block/book-review', {
 		);
 	},
 
-	save: ( { attributes } ) => {
-		if ( ! isOldVersion( attributes ) ) {
-			return null;
-		}
+	save: () => {
+		return null;
+	},
 
-		const {
-			alt,
-			author = [],
-			backgroundColor,
-			summary = [],
-			format = [],
-			genre = [],
-			pages = [],
-			publisher = [],
-			rating: reviewRating,
-			releaseDate = [],
-			series = [],
-			source = [],
-			title = [],
-			url,
-		} = attributes;
-		const className = classnames( 'book-review-block', {
-			'has-background': backgroundColor,
-		} );
-		const styles = {
-			backgroundColor: backgroundColor,
-		};
+	deprecated: [ {
+		attributes: {
+			alt: {
+				type: 'string',
+				source: 'attribute',
+				selector: 'img',
+				attribute: 'alt',
+			},
+			author: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__author',
+			},
+			backgroundColor: {
+				type: 'string',
+			},
+			format: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__format',
+			},
+			genre: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__genre',
+			},
+			pages: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__pages',
+			},
+			publisher: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__publisher',
+			},
+			rating: {
+				type: 'string',
+			},
+			releaseDate: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__release-date',
+			},
+			series: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__series',
+			},
+			source: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__source',
+			},
+			summary: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__description',
+			},
+			title: {
+				type: 'array',
+				source: 'children',
+				selector: '.book-review-block__title',
+			},
+			url: {
+				type: 'string',
+				source: 'attribute',
+				selector: 'img',
+				attribute: 'src',
+			},
+		},
 
-		return (
-			<div
-				className={ className }
-				style={ styles }>
-				{ !! url && (
-					<div className="book-review-block__cover-wrapper">
-						<img className="book-review-block__cover" src={ url } alt={ alt } />
+		save( { attributes } ) {
+			const {
+				alt,
+				author = [],
+				backgroundColor,
+				format = [],
+				genre = [],
+				pages = [],
+				publisher = [],
+				rating: reviewRating,
+				releaseDate = [],
+				series = [],
+				source = [],
+				summary = [],
+				title = [],
+				url,
+			} = attributes;
+			const className = classnames( 'book-review-block', {
+				'has-background': backgroundColor,
+			} );
+			const styles = {
+				backgroundColor: backgroundColor,
+			};
+
+			return (
+				<div
+					className={ className }
+					style={ styles }>
+					{ !! url && (
+						<div className="book-review-block__cover-wrapper">
+							<img className="book-review-block__cover" src={ url } alt={ alt } />
+						</div>
+					) }
+					<div className="book-review-block__details">
+						{ title.length > 0 && <span className="book-review-block__title">{ title }</span> }
+						{ series.length > 0 && <span className="book-review-block__series">{ series }</span> }
+						{ author.length > 0 && <span className="book-review-block__author">{ author }</span> }
+						{ genre.length > 0 && <span className="book-review-block__genre">{ genre }</span> }
+						{ publisher.length > 0 && <span className="book-review-block__publisher">{ publisher }</span> }
+						{ releaseDate.length > 0 && <span className="book-review-block__release-date">{ releaseDate }</span> }
+						{ format.length > 0 && <span className="book-review-block__format">{ format }</span> }
+						{ pages.length > 0 && <span className="book-review-block__pages">{ pages }</span> }
+						{ source.length > 0 && <span className="book-review-block__source">{ source }</span> }
+
+						<div className="book-review-block__rating">
+							{ ratings.map( ( { rating } ) => (
+								<span
+									className={ reviewRating && rating <= reviewRating ? 'on' : 'off' }
+									data-rating={ rating }
+									key={ rating }>
+									&#9734;
+								</span>
+							) ) }
+						</div>
+
+						{ summary.length > 0 && <div className="book-review-block__description">{ summary }</div> }
 					</div>
-				) }
-				<div className="book-review-block__details">
-					{ title.length > 0 && <span className="book-review-block__title">{ title }</span> }
-					{ series.length > 0 && <span className="book-review-block__series">{ series }</span> }
-					{ author.length > 0 && <span className="book-review-block__author">{ author }</span> }
-					{ genre.length > 0 && <span className="book-review-block__genre">{ genre }</span> }
-					{ publisher.length > 0 && <span className="book-review-block__publisher">{ publisher }</span> }
-					{ releaseDate.length > 0 && <span className="book-review-block__release-date">{ releaseDate }</span> }
-					{ format.length > 0 && <span className="book-review-block__format">{ format }</span> }
-					{ pages.length > 0 && <span className="book-review-block__pages">{ pages }</span> }
-					{ source.length > 0 && <span className="book-review-block__source">{ source }</span> }
-
-					<div className="book-review-block__rating">
-						{ ratings.map( ( { rating } ) => (
-							<span
-								className={ reviewRating && rating <= reviewRating ? 'on' : 'off' }
-								data-rating={ rating }
-								key={ rating }>
-								&#9734;
-							</span>
-						) ) }
-					</div>
-
-					{ summary.length > 0 && <div className="book-review-block__description">{ summary }</div> }
 				</div>
-			</div>
-		);
-	}
+			);
+		},
+	} ],
 } );

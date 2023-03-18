6e733d5f4d1eba948cd260793a33ba910414dd66
Donna Peplinskie <donnapep@gmail.com>
2023-03-18 12:14:18 -0400

    Simplify ESLint config as rules are included in WP recommended ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,59 +1,11 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "globals": {
-        "jQuery": true,
-        "ajaxurl": true,
-        "woo_localized_data": true,
-        "wp": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        }
-    },
-    "plugins": [
-        "react"
-    ],
-    "settings": {
-        "react": {
-            "pragma": "wp"
-        }
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "jsx-quotes": "error",
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "react/display-name": "off",
-        "react/jsx-curly-spacing": [ "error", "always" ],
-        "react/jsx-equals-spacing": "error",
-        "react/jsx-indent": [ "error", "tab" ],
-        "react/jsx-indent-props": [ "error", "tab" ],
-        "react/jsx-key": "error",
-        "react/jsx-tag-spacing": "error",
-        "react/no-children-prop": "off",
-        "react/no-find-dom-node": "warn",
-        "react/prop-types": "off",
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	"extends": [ "plugin:@wordpress/eslint-plugin/recommended" ],
+	"rules": {
+		"@wordpress/i18n-text-domain": [
+			"error",
+			{
+				"allowedTextDomain": "book-review-block"
+			}
+		]
+	}
 };

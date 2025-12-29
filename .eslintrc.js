a8be009614662a3712bbe6c2babe88e8e03ffda6
Donna Peplinskie <donna.peplinskie@automattic.com>
2025-12-28 21:02:53 -0500

    Update linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-	"extends": [ "plugin:@wordpress/eslint-plugin/recommended" ],
-	"rules": {
-		"@wordpress/i18n-text-domain": [
-			"error",
+	...require( '@wordpress/scripts/config/.eslintrc.js' ),
+	rules: {
+		'@wordpress/i18n-text-domain': [
+			'error',
 			{
-				"allowedTextDomain": "book-review-block"
-			}
-		]
-	}
+				allowedTextDomain: 'book-review-block',
+			},
+		],
+		'react-hooks/exhaustive-deps': 'warn',
+	},
 };
